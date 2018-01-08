a97c58cc68cc2e66c90ef1dc6ecbf29b162094f6
Alex Parij <alex@gazelle.company>
2018-01-08 18:01:23 -0500

    *add top20 tags response

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 import express from 'express';
 import tagData from './public/tagdata.json';
+import _ from 'lodash';
 
 const app = express()
 
+const top20 = _.sortBy(Object.keys(tagData).filter(key=>tagData[key][tagData[key].length -1].rank<21)
+                    .map(key=>{return {name: key, value: tagData[key][tagData[key].length -1 ]}}),
+                    ['value.rank']);
+                    
+
 app.get('/tags', (req, res) => {
-  const queryLength=req.query.keywords.length;
-  const tags = req.query.keywords.split(",");
   let responseData={};
-  for(let tag of tags){
-    responseData[tag]=tagData[tag];
+  if(req.query.keywords){
+    const queryLength = req.query.keywords.length;
+    const tags = req.query.keywords.split(",");
+    for(let tag of tags){
+      responseData[tag]=tagData[tag];
+    }
+  }else{
+    responseData = top20;
   }
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   res.json({data:responseData})
 }
 
